9ac06e767f70711ada71da224263a43eeef20261
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2023-05-29 14:08:56 -0300

    Theme config

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from "react";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 function App() {
   return (
     <>
+    <ThemeProvider theme={temaClaro}>
       <GlobalStyle />
       <Cabecalho />
       <Container />
+    </ThemeProvider>
     </>
   );
 }
